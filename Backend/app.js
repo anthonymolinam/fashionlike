3a8289a4af9edc72ceb26a4fe83adadd2e878999
Alberto Gonzalez <alberto06181@gmail.com>
2023-03-16 20:55:10 -0500

    port change

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = process.env.POST || 3000
+const port = process.env.POST || 4000
 
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
 
 app.listen(port, () => {
-    console.log(`Express app is listening on ${port}`);
+    console.log(`Express app is listening on port ${port}`);
 })
