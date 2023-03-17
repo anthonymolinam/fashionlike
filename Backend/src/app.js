c1f006402eb5b98460407b500f90c98614a3fd17
Alberto Gonzalez <alberto06181@gmail.com>
2023-03-17 15:33:17 -0500

    restructuring files and folders

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,10 +6,11 @@ import cors from 'cors'
 
 import userAuth_routes from './routes/user_auth.route.js'
 
+// initialization
 const app = express()
 const port = process.env.POST || 4000
 
-// settings
+// middlewares
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -21,6 +22,4 @@ app.get('/api', (req, res) => {
 // app.use('/api/docs', docs)
 app.use('/api/user', userAuth_routes)
 
-app.listen(port, () => {
-    console.log(`Express is now running on localhost:${port}`);
-})
+export default app
---
+++ Backend/src/index.js
@@ -0,0 +1,10 @@
+import app from './app'
+
+async function main(port) {
+    await app.listen(port, () => {
+        console.log(`Express is now running on http://localhost:${port}`);
+    })
+}
+
+const PORT = process.env.PORT || 4000
+main(PORT)
