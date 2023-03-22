5971966ea80cacabfc0cae31f259b43d1d12d24c
Alberto Gonzalez <alberto06181@gmail.com>
2023-03-21 20:32:32 -0500

    models

--- Backend/src/models/publication.js
+++
@@ -1,4 +0,0 @@
-export const pubTable = "CREATE TABLE IF NOT EXISTS publication(" +
-    "userId INTEGER REFERENCES users(id)," +
-    "description VARCHAR(300)," +
-    "daterelease DATE NOT NULL DEFAULT CURRENT_DATE)"
