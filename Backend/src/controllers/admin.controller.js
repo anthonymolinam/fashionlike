a0457273a7cbb100f454bac2a650b044c874988a
Alberto Gonzalez <alberto06181@gmail.com>
2023-04-28 18:44:56 -0500

    admin controller

---
+++ Backend/src/controllers/admin.controller.js
@@ -0,0 +1,51 @@
+const PostSchema = require('../models/post')
+const UserSchema = require('../models/user')
+
+const editPost = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { description } = req.body
+        await PostSchema.update({ description }, { where: { id } })
+        res.status(200).json({ message: 'Publication has been edited' })
+    } catch (e) {
+        res.status(200).json({ error: 'This publication id does not exist' })
+    }
+}
+
+const editUser = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { username, email, password, verified, role } = req.body
+        await UserSchema.update({ username, email, password, verified, role }, { where: { id } })
+        res.status(200).json({ message: 'User has been edited' })
+    } catch (e) {
+        res.status(200).json({ error: 'This user id does not exist' })
+    }
+}
+
+const deletePost = async (req, res) => {
+    try {
+        const { id } = req.params
+        await PostSchema.destroy({ where: { id } })
+        res.status(200).json({ message: 'Publication has been deleted' })
+    } catch (e) {
+        res.status(404).json({ error: 'This publication id does not exist' })
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params
+        await UserSchema.destroy({ where: { id } })
+        res.status(200).json({ message: 'User has been deleted' })
+    } catch (e) {
+        res.status(404).json({ error: 'This user id does not exist' })
+    }
+}
+
+module.exports = {
+    editPost,
+    editUser,
+    deletePost,
+    deleteUser
+}
--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
+
 const { checkRole } = require('../helpers/check_auth')
+const { editPost, editUser, deletePost, deleteUser } = require('../controllers/admin.controller')
 
-router.put('/update/post', checkRole(['admin']), )
-router.put('/update/user', checkRole(['admin']), )
+router.put('/update/post/:id', checkRole(['admin']), editPost)
+router.put('/update/user/:id', checkRole(['admin']), editUser)
 
-router.delete('/delete/user', checkRole(['admin']), )
-router.delete('/delete/post', checkRole(['admin']), )
+router.delete('/delete/post/:id', checkRole(['admin']), deletePost)
+router.delete('/delete/user/:id', checkRole(['admin']), deleteUser)
 
 module.exports = router
